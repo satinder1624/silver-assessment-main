a58bcfc7cd520828f2ae9a58f26668803908f962
satinder <satinder1624@gmail.com>
2022-08-30 15:18:17 -0400

    Fetching post on client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 export function App() {
+  const [repos, setRepos] = useState<any[]>([]);
+
+  useEffect(() => {
+    let isCancelled = true;
+    axios
+      .get('http://localhost:4000/repos', {
+        headers: {
+          'content-type': 'application/json',
+        },
+      })
+      .then((result) => {
+        if (isCancelled) {
+          // console.log(result.data);
+          setRepos([result.data]);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return () => {
+      isCancelled = false;
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's see</h1>
+      {repos[0]?.map((repo: any, i: any) => (
+        <div key={i}>{repo?.id}</div>
+      ))}
     </div>
   );
 }
